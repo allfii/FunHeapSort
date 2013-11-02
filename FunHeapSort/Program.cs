aa352e0807889df7ed1db35a1d532fcab072ff80
masphei <masphei@gmail.com>
2013-11-02 09:34:25 +0700

    Modify Algorithm
    Signed-off-by: masphei <masphei@gmail.com>

--- FunHeapSort/Program.cs
+++ FunHeapSort/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace FunHeapSort
 {
@@ -9,6 +10,33 @@ namespace FunHeapSort
     {
         static void Main(string[] args)
         {
+            int n = 350000;
+            int[] array = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(n*rand.Next(50));
+            }
+            Console.WriteLine("-----------HeapSort-------------");
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " | ");
+            Console.WriteLine();
+
+            DateTime startTime;
+            startTime = DateTime.Now;
+            HeapSort HS = new HeapSort();
+            HS.sort(array);
+
+            DateTime endTime = DateTime.Now;
+            TimeSpan diff = endTime - startTime;
+
+            Console.WriteLine("-----------Result-------------");
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " | ");
+            Console.WriteLine();
+            Console.WriteLine("-----------Finish-------------");
+            Console.WriteLine((int)Math.Round((double)diff.Seconds, 0, MidpointRounding.AwayFromZero) + ":" + (int)Math.Round((double)diff.Milliseconds, 0, MidpointRounding.AwayFromZero));
+            Console.ReadLine();
         }
     }
 }
